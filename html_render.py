1452b76e1a6a0529c755f65b28b2e13676dd1cdc
Mark Coppa <markcoppa@gmail.com>
2025-06-13 11:04:35 -0700

    Add head, title, and one liner classes

--- html_render.py
+++ html_render.py
@@ -28,12 +28,9 @@ class Element:
         self.write_opening_tag(out_file)
         for item in self._content:
             mro = str(item.__class__.__mro__[-2])
-#            if mro == "<class '__main__.Element'>" or mro == "<class 'html_render.Element'>":
             if mro.__contains__(".Element'>"):
-#                print("rendering element class")
                 item.render(out_file)
             else:
-#                print("rendering string")
                 out_file.write(item + "\n")
         self.write_closing_tag(out_file)
     
@@ -68,6 +65,55 @@ class P(Element):
     def __init__(self, content=None):
         super().__init__(content=content, tag="p")
 
+class Head(Element):
+
+    def __init__(self, content=None):
+        super().__init__(content=content, tag="head")
+
+class OneLineTag(Element):
+    """
+    A subclass that overrides render(), such that it prints the element all on one line.
+    Note that the only difference is that the line feeds for the opening tag
+    and the content are taken out.
+    Some refactoring can be done here.
+    """
+
+    def __init__(self, content=None, tag=""):
+        super().__init__(content=content, tag=tag)
+
+    def render(self, out_file):
+        self.write_opening_tag(out_file)
+        for item in self._content:
+            mro = str(item.__class__.__mro__[-2])
+            if mro.__contains__(".Element'>"):
+                item.render(out_file)
+            else:
+                out_file.write(item)
+        self.write_closing_tag(out_file)
+    
+    def write_opening_tag(self, out_file):
+        if self._tag == "None":
+            pass
+        elif self._tag:
+            out_file.write(f"<{self._tag}>")
+        else:
+            out_file.write(f"<html>")
+    
+    def write_closing_tag(self, out_file):
+        if self._tag == "None":
+            pass
+        elif self._tag:
+            out_file.write(f"</{self._tag}>\n")
+        else:
+            out_file.write(f"</html>\n")
+
+class Title(OneLineTag):
+
+    def __init__(self, content=None):
+        super().__init__(content=content, tag="title")
+
+
+
 
 import io
 
@@ -90,26 +136,17 @@ def render_result(element, ind=""):
 
 
 if __name__ == "__main__":
-    """
     top = Element("None")
     top.append("<!DOCTYPE html>")
-    h = Html()
-    top.append(h)
+    head = Head()
+    top.append(head)
+    html = Html()
+    html.append(Title("Title goes here"))
+    head.append(html)
     e = Body("this is some text")
     e.append("and this is some more text")
-    h.append(e)
-    """
+    html.append(e)
 
-    # This uses the render_results utility above
-    page = Html()
-    page.append("some plain text.")
-    page.append(P("A simple paragraph of text"))
-    page.append("Some more plain text.")
-
-    for item in page._content:
-        mro = str(item.__class__.__mro__[-2])
-        print(f"item: {item}  mro: {mro}")
-
-
-    file_contents = render_result(page)
-    print(file_contents) # so we can see it if the test fails
+    outfile = io.StringIO()
+    top.render(outfile)
+    print(outfile.getvalue())
